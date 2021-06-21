bda43dfd2ce07c74b238a095997b1ea94baebdc9
JBayona <jordavids_22@hotmail.com>
2021-06-20 20:33:09 -0500

    Add VPC resource to our stack.

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require("@aws-cdk/core");
+import ec2 = require("@aws-cdk/aws-ec2");
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // The resources of your stack goes here
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Isolated",
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
+    });
   }
 }
